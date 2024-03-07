8b1e56eb9d56beed021e87570a8c2b6c2c935a16
pikachu669 <132365833+pikachu669@users.noreply.github.com>
2024-03-07 17:25:40 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -210,7 +210,7 @@ def get_readable_message():
         buttons.ibutton("Prev", "status pre")
         buttons.ibutton(f"{PAGE_NO}/{PAGES}", "status ref")
         buttons.ibutton("Next", "status nex")
-        buttons.ibutton("Owner", "t.me/killerboy098")
+        buttons.ibutton("Owner", "https://t.me/killerboy098")
         button = buttons.build_menu(3)
     msg += f"<b>⛩️ Tasks</b>: {tasks}{bmax_task}"
     msg += f"\n<b>⛩️ Bot uptime</b>: {currentTime}"
